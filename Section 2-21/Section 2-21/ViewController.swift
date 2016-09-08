506fe1bced6956d23ba96916d984bb24ff07500d
Ilias Alexopoulos <ilias.alexopoulos@workangel.com>
2016-09-08 15:24:35 +0100

    remove files

---
+++ Section 2-21/Section 2-21/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Section 2-21
+//
+//  Created by Ilias Alexopoulos on 08/09/2016.
+//  Copyright © 2016 WorkAngel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelHello: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
